eaa2db91aafa6f75782b302f9af3b257a5b37be0
Michal Tomek <tomekmichalhu@seznam.cz>
2023-08-29 10:41:07 +0200

    USB update, show system panel on new update

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -20,6 +20,8 @@ class BasePanel(ScreenPanel):
         self.time_min = -1
         self.time_format = self._config.get_main_config().getboolean("24htime", True)
         self.time_update = None
+        self.updater_update = None
+        self.last_update_status = None
         self.titlebar_items = []
         self.titlebar_name_type = None
         self.buttons_showing = {
@@ -175,6 +177,9 @@ class BasePanel(ScreenPanel):
         if self.time_update is None:
             self.time_update = GLib.timeout_add_seconds(1, self.update_time)
 
+        if self.updater_update is None:
+            self.updater_update = GLib.timeout_add_seconds(3, self.update_updater)
+
     def add_content(self, panel):
         self.current_panel = panel
         self.set_title(panel.title)
@@ -300,6 +305,19 @@ class BasePanel(ScreenPanel):
                 self.control['time'].set_text(f'{now:%I:%M %p}')
             self.time_min = now.minute
             self.time_format = confopt
+        return True
+
+    def update_updater(self):
+        logging.info("Updater check from base panel...")
+        update_resp = self._screen.tpcclient.send_request(f"check_update")
+        update_status = update_resp["update_status"]
+
+        if update_status != self.last_update_status:
+            self.last_update_status = update_status
+            if update_status == "UPDATE_AVAILABLE" \
+                    or update_status == "INSTALLED" \
+                    or update_status == "USB_UPDATE_AVAILABLE":
+                self._screen.show_panel("system", "system", "System", 1, False)
         return True
 
     def show_estop(self, show=True):

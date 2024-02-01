3ca038b7358ffcf1b0e755a65e02432e08a3c685
Michal Tomek <tomekmichalhu@seznam.cz>
2024-02-01 11:08:39 +0100

    Load/unload wizard do not set target temperature bellow actual target

--- WizardSteps/loadWizardSteps.py
+++ WizardSteps/loadWizardSteps.py
@@ -82,14 +82,15 @@ class SelectFilament(BaseWizardStep):
         if len(self.heaters) == 0:
             self._screen.show_popup_message(_("Nothing selected"))
         else:
+            logging.info(f"{self.preheat_options[setting]}")
             for heater in self.heaters:
                 logging.info(f"Looking for settings for heater {heater}")
                 target = None
                 max_temp = float(self._screen.printer.get_config_section(heater)['max_temp'])
+                target_actual = self._screen.printer.data[heater]["target"]
                 name = heater.split()[1] if len(heater.split()) > 1 else heater
                 with contextlib.suppress(KeyError):
                     for i in self.preheat_options[setting]:
-                        logging.info(f"{self.preheat_options[setting]}")
                         if i == name:
                             # Assign the specific target if available
                             target = self.preheat_options[setting][name]
@@ -100,31 +101,31 @@ class SelectFilament(BaseWizardStep):
                 if target is None and setting == "cooldown" and not heater.startswith('temperature_fan '):
                     target = 0
                 if heater.startswith('extruder'):
-                    if self.validate(heater, target, max_temp):
+                    if self.validate(heater, target, max_temp, target_actual):
                         self._screen._ws.klippy.set_tool_temp(self._screen.printer.get_tool_number(heater), target)
                 elif heater.startswith('heater_bed'):
                     if target is None:
                         with contextlib.suppress(KeyError):
                             target = self.preheat_options[setting]["bed"]
-                    if self.validate(heater, target, max_temp):
+                    if self.validate(heater, target, max_temp, target_actual):
                         self._screen._ws.klippy.set_bed_temp(target)
                 elif heater.startswith('heater_chamber'):
                     if target is None:
                         with contextlib.suppress(KeyError):
                             target = self.preheat_options[setting]["chamber"]
-                    if self.validate(heater, target, max_temp):
+                    if self.validate(heater, target, max_temp, target_actual):
                         self._screen._ws.klippy.set_chamber_temp(target)
                 elif heater.startswith('heater_generic '):
                     if target is None:
                         with contextlib.suppress(KeyError):
                             target = self.preheat_options[setting]["heater_generic"]
-                    if self.validate(heater, target, max_temp):
+                    if self.validate(heater, target, max_temp, target_actual):
                         self._screen._ws.klippy.set_heater_temp(name, target)
                 elif heater.startswith('temperature_fan '):
                     if target is None:
                         with contextlib.suppress(KeyError):
                             target = self.preheat_options[setting]["temperature_fan"]
-                    if self.validate(heater, target, max_temp):
+                    if self.validate(heater, target, max_temp, target_actual):
                         self._screen._ws.klippy.set_temp_fan_temp(name, target)
 
         self.wizard_manager.set_step(self.next_step)
@@ -135,6 +136,10 @@ class SelectFilament(BaseWizardStep):
                 speed_request = 1
 
     def validate(self, heater, target=None, max_temp=None):
+    def validate(self, heater, target=None, max_temp=None, target_actual=None):
+        if target is not None and target_actual is not None and target <= target_actual:
+            logging.debug(f"Actual target {target_actual} is greater or equal than target {target}. Skipping.")
+            return False
         if target is not None and max_temp is not None:
             if 0 <= target <= max_temp:
                 self._screen.printer.set_dev_stat(heater, "target", target)

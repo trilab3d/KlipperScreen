f16d3d2b2396bca5f107aa0ce6ec6bd8622a40cd
Michal Tomek <tomekmichalhu@seznam.cz>
2024-04-29 14:28:24 +0200

    Feature "do not set lower temperature on load" removed

--- WizardSteps/wizardCommons.py
+++ WizardSteps/wizardCommons.py
@@ -11,7 +11,6 @@ class TemperatureSetter:
                 logging.info(f"Looking for settings for heater {heater}")
                 target = None
                 max_temp = float(self._screen.printer.get_config_section(heater)['max_temp'])
-                target_actual = self._screen.printer.data[heater]["target"]
                 name = heater.split()[1] if len(heater.split()) > 1 else heater
                 with contextlib.suppress(KeyError):
                     for i in self.preheat_options[setting]:
@@ -25,34 +24,31 @@ class TemperatureSetter:
                 if target is None and setting == "cooldown" and not heater.startswith('temperature_fan '):
                     target = 0
                 if heater.startswith('extruder'):
-                    if setting == 'cooldown' or self.validate(heater, target, max_temp, target_actual,
-                                                              self.preheat_options[setting]["extruder_max"]
-                                                              if "extruder_max" in self.preheat_options[setting]
-                                                              else None):
+                    if setting == 'cooldown' or self.validate(heater, target, max_temp):
                         self._screen._ws.klippy.set_tool_temp(self._screen.printer.get_tool_number(heater), target)
                 elif heater.startswith('heater_bed'):
                     if target is None:
                         with contextlib.suppress(KeyError):
                             target = self.preheat_options[setting]["bed"]
-                    if setting == 'cooldown' or self.validate(heater, target, max_temp, target_actual):
+                    if setting == 'cooldown' or self.validate(heater, target, max_temp):
                         self._screen._ws.klippy.set_bed_temp(target)
                 elif heater.startswith('heater_chamber'):
                     if target is None:
                         with contextlib.suppress(KeyError):
                             target = self.preheat_options[setting]["chamber"]
-                    if setting == 'cooldown' or self.validate(heater, target, max_temp, target_actual):
+                    if setting == 'cooldown' or self.validate(heater, target, max_temp):
                         self._screen._ws.klippy.set_chamber_temp(target)
                 elif heater.startswith('heater_generic '):
                     if target is None:
                         with contextlib.suppress(KeyError):
                             target = self.preheat_options[setting]["heater_generic"]
-                    if setting == 'cooldown' or self.validate(heater, target, max_temp, target_actual):
+                    if setting == 'cooldown' or self.validate(heater, target, max_temp):
                         self._screen._ws.klippy.set_heater_temp(name, target)
                 elif heater.startswith('temperature_fan '):
                     if target is None:
                         with contextlib.suppress(KeyError):
                             target = self.preheat_options[setting]["temperature_fan"]
-                    if setting == 'cooldown' or self.validate(heater, target, max_temp, target_actual):
+                    if setting == 'cooldown' or self.validate(heater, target, max_temp):
                         self._screen._ws.klippy.set_temp_fan_temp(name, target)
 
             if setting == 'cooldown':
@@ -65,14 +61,10 @@ class TemperatureSetter:
             else:
                 speed_request = 1
 
-    def validate(self, heater, target=None, max_temp=None, target_actual=None, target_max=None):
-        if (target is not None and target_actual is not None and target <= target_actual and
-                (target_max is None or target_max > target_actual)):
-            logging.debug(f"Actual target {target_actual} is greater or equal than target {target}. Skipping.")
-            return False
+    def validate(self, heater, target=None, max_temp=None):
         if target is not None and max_temp is not None:
             if 0 <= target <= max_temp:
-                self._screen.printer.set_dev_stat(heater, "target", target)
+                self._printer.set_dev_stat(heater, "target", target)
                 return True
             elif target > max_temp:
                 self._screen.show_popup_message(_("Can't set above the maximum:") + f' {max_temp}')

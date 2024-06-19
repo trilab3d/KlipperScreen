e4b40ef43a79a94016d13bc09a0b307fbdcb6eb9
Michal Tomek <tomekmichalhu@seznam.cz>
2024-06-19 11:38:25 +0200

    Software update on the end of init wizard

--- WizardSteps/InitialSetupSteps.py
+++ WizardSteps/InitialSetupSteps.py
@@ -427,7 +427,7 @@ class Privacy(BaseWizardStep):
             "privacy": PRESET_FIELDS["reduced"],
         }
         requests.post("http://127.0.0.1/tpc/settings", json=settings)
-        self.wizard_manager.set_step(Done(self._screen))
+        self.wizard_manager.set_step(PrinterName(self._screen))
 
     def recommended_pressed(self, widget):
         settings = {
@@ -636,7 +636,7 @@ class PrusaConnectDialog(BaseWizardStep):
         self.wizard_manager.set_step(PrusaConnectInProgress(self._screen))
 
     def skip_pressed(self, widget):
-        self.wizard_manager.set_step(Done(self._screen))
+        self.wizard_manager.set_step(PreUpdate(self._screen))
 
 class PrusaConnectInProgress(BaseWizardStep):
     def __init__(self, screen):
@@ -660,7 +660,7 @@ class PrusaConnectInProgress(BaseWizardStep):
         except:
             self._screen.show_popup_message("Error on PrusaConnect module, skipping PrusaConnect configuration. "
                                             "You can configure it later.", 2)
-            self.wizard_manager.set_step(Done(self._screen))
+            self.wizard_manager.set_step(PreUpdate(self._screen))
             return
         self.content = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
 
@@ -710,7 +710,7 @@ class PrusaConnectInProgress(BaseWizardStep):
 class PrusConnectDone(BaseWizardStep):
     def __init__(self, screen):
         super().__init__(screen)
-        self.can_back = True
+        self.can_home = False
 
     def activate(self, wizard):
         super().activate(wizard)
@@ -726,6 +726,317 @@ class PrusConnectDone(BaseWizardStep):
         button.set_vexpand(False)
         button.connect("clicked", self.continue_pressed)
         self.content.add(button)
+
+    def continue_pressed(self, widget):
+        self.wizard_manager.set_step(PreUpdate(self._screen))
+
+class PreUpdate(BaseWizardStep):
+    def __init__(self, screen):
+        super().__init__(screen)
+        self.can_back = False
+        self.can_home = False
+
+    def activate(self, wizard):
+        super().activate(wizard)
+        self.content = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
+        img = self._screen.gtk.Image("info", self._screen.gtk.content_width * .945, 450)
+        self.content.add(img)
+        label = self._screen.gtk.Label("")
+        label.set_margin_top(20)
+        label.set_markup(
+            "<span size='large'>" + _("Software update") + "</span>")
+        self.content.add(label)
+        if not self.wizard_manager.get_wizard_data("network_working"):
+            label = self._screen.gtk.Label("")
+            label.set_margin_top(20)
+            label.set_markup(
+                "<span size='small'>" + _("Insert provided USB disk with software update") + "</span>")
+            self.content.add(label)
+        button = self._screen.gtk.Button(label=_("Check For Updates"), style=f"color1")
+        button.set_vexpand(False)
+        button.connect("clicked", self.continue_pressed)
+        self.content.add(button)
+        button = self._screen.gtk.Button(label=_("Skip Software Update"), style=f"color1")
+        button.set_vexpand(False)
+        button.connect("clicked", self.skip_pressed)
+        self.content.add(button)
+
+    def continue_pressed(self, widget):
+        os.system("echo CheckForUpdates > /opt/init_state")
+        self.wizard_manager.set_step(CheckForUpdates(self._screen))
+
+    def skip_pressed(self, widget):
+        self.wizard_manager.set_step(PreUpdateSkip(self._screen))
+
+class PreUpdateSkip(BaseWizardStep):
+    def __init__(self, screen):
+        super().__init__(screen)
+        self.can_back = False
+        self.can_home = False
+
+    def activate(self, wizard):
+        super().activate(wizard)
+        self.content = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
+        img = self._screen.gtk.Image("warning", self._screen.gtk.content_width * .945, 450)
+        self.content.add(img)
+        label = self._screen.gtk.Label("")
+        label.set_margin_top(20)
+        label.set_line_wrap_mode(Pango.WrapMode.WORD_CHAR)
+        label.set_line_wrap(True)
+        label.set_markup(
+            "<span size='large'>" + _("Software updates are important for security and bug fixes. "
+                                      "We recommend to always run you printer with latest software") + "</span>")
+        self.content.add(label)
+        button = self._screen.gtk.Button(label=_("Check For Updates"),
+                                         style=f"color1")
+        button.set_vexpand(False)
+        button.connect("clicked", self.continue_pressed)
+        self.content.add(button)
+        button = self._screen.gtk.Button(label=_("Skip Anyway"), style=f"color1")
+        button.set_vexpand(False)
+        button.connect("clicked", self.skip_pressed)
+        self.content.add(button)
+
+    def continue_pressed(self, widget):
+        os.system("echo CheckForUpdates > /opt/init_state")
+        self.wizard_manager.set_step(CheckForUpdates(self._screen))
+
+    def skip_pressed(self, widget):
+        self.wizard_manager.set_step(Done(self._screen))
+
+class CheckForUpdates(BaseWizardStep):
+    def __init__(self, screen):
+        super().__init__(screen)
+        self.can_exit = False
+
+    def activate(self, wizard):
+        super().activate(wizard)
+        self._screen.tpcclient.send_request(f"refresh_updater", "POST")
+        self.refresh = None
+        self.update_dialog = None
+        grid = self._screen.gtk.HomogeneousGrid()
+        grid.set_row_homogeneous(False)
+        self.do_schedule_refresh = True
+        self.service_sequence = ""
+
+        self.download_button = self._screen.gtk.Button('arrow-down', _('Download'), 'color1')
+        self.download_button.connect("clicked", self.download)
+        self.download_button.set_vexpand(False)
+        self.download_button.set_hexpand(True)
+
+        self.retry_button = self._screen.gtk.Button('refresh', _('Retry'), 'color1')
+        self.retry_button.connect("clicked", self.download)
+        self.retry_button.set_vexpand(False)
+        self.retry_button.set_hexpand(True)
+
+        self.update_button = self._screen.gtk.Button('arrow-up', _('Update'), 'color2')
+        self.update_button.connect("clicked", self.perform_update)
+        self.update_button.set_vexpand(False)
+        self.update_button.set_hexpand(True)
+
+        self.install_usb_button = self._screen.gtk.Button('arrow-right', _('Install'), 'color1')
+        self.install_usb_button.connect("clicked", self.install_usb_update)
+        self.install_usb_button.set_vexpand(False)
+        self.install_usb_button.set_hexpand(True)
+
+        self.continue_button = self._screen.gtk.Button('arrow-right', _('Continue'), 'color1')
+        self.continue_button.connect("clicked", self.continue_pressed)
+        self.continue_button.set_vexpand(False)
+        self.continue_button.set_hexpand(True)
+
+        scroll = self._screen.gtk.ScrolledWindow()
+        scroll.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+
+        infogrid = Gtk.Grid()
+        infogrid.get_style_context().add_class("system-program-grid")
+
+        self.icon_ok = self._screen.gtk.Image("complete", self._screen.gtk.content_width * .9, self._screen.gtk.content_height * .4)
+        self.icon_update = self._screen.gtk.Image("update-available", self._screen.gtk.content_width * .9,
+                                           self._screen.gtk.content_height * .4)
+        self.icon_update_usb = self._screen.gtk.Image("update-available-usb", self._screen.gtk.content_width * .9,
+                                               self._screen.gtk.content_height * .4)
+        self.icon_downloading = self._screen.gtk.Image("update-downloading", self._screen.gtk.content_width * .9,
+                                                self._screen.gtk.content_height * .4)
+        self.icon_unpacking = self._screen.gtk.Image("unpacking", self._screen.gtk.content_width * .9, self._screen.gtk.content_height * .4)
+        self.icon_unpacking_usb = self._screen.gtk.Image("unpacking-usb", self._screen.gtk.content_width * .9,
+                                                  self._screen.gtk.content_height * .4)
+        self.icon_installed = self._screen.gtk.Image("info", self._screen.gtk.content_width * .9, self._screen.gtk.content_height * .4)
+        self.icon_installed_usb = self._screen.gtk.Image("info", self._screen.gtk.content_width * .9, self._screen.gtk.content_height * .4)
+        self.icon_warning = self._screen.gtk.Image("warning", self._screen.gtk.content_width * .9, self._screen.gtk.content_height * .4)
+
+        self.icon_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        self.icon_box.set_hexpand(True)
+        self.icon_box.set_vexpand(False)
+        self.icon_box.set_homogeneous(True)
+        self.icon_box.add(self.icon_installed)
+
+        self.update_header = Gtk.Label()
+        self.update_header.set_hexpand(True)  # align to center
+        self.update_header.set_margin_top(40)
+        self.update_header.set_markup("<span size='xx-large'>" + _("Checking for update") + "</span>")
+        self.update_label = Gtk.Label()
+        self.update_label.set_hexpand(False)
+        self.update_label.set_halign(Gtk.Align.START)
+        self.update_label.set_margin_top(5)
+        self.update_label.set_line_wrap(True)
+        self.release_notes_label = Gtk.Label()
+        self.release_notes_label.set_line_wrap(True)
+        # self.release_notes_label.set_margin_top(5)
+        self.release_notes_label.set_halign(Gtk.Align.START)
+
+        self.progress = Gtk.ProgressBar()
+        self.progress.set_fraction(0)
+        self.progress.set_show_text(False)
+        self.progress.set_hexpand(True)
+        # self.progress.get_style_context().add_class("progressbar_thin")
+
+        self.progress_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        self.progress_box.set_hexpand(True)
+        self.progress_box.set_vexpand(False)
+        self.progress_box.set_homogeneous(True)
+
+        self.button_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.button_box.set_hexpand(True)
+        self.button_box.set_vexpand(False)
+        self.button_box.set_homogeneous(True)
+
+        event_box_icon = Gtk.EventBox()
+        event_box_icon.set_margin_top(20)
+        event_box_icon.add(self.icon_box)
+
+        event_box_labels = Gtk.EventBox()
+        grid_text = Gtk.Grid()
+        grid_labels = Gtk.Grid()
+        grid_labels.set_halign(Gtk.Align.CENTER)
+        event_box_labels.set_margin_top(20)
+        event_box_labels.add(grid_text)
+        # grid_text.attach(self.update_header, 0, 0, 1, 1)
+        grid_text.attach(grid_labels, 0, 1, 1, 1)
+        grid_labels.attach(self.progress_box, 0, 0, 1, 1)
+        grid_labels.attach(self.update_label, 0, 1, 1, 1)
+        grid_labels.attach(self.release_notes_label, 0, 2, 1, 1)
+
+        infogrid.attach(event_box_icon, 0, 0, 1, 1)
+        infogrid.attach(self.update_header, 0, 1, 1, 1)
+        infogrid.attach(event_box_labels, 0, 2, 1, 1)
+
+        scroll.add(infogrid)
+
+        grid.attach(scroll, 0, 0, 1, 1)
+        grid.attach(self.button_box, 0, 1, 1, 1)
+
+        self.content = grid
+
+    def update_loop(self):
+        try:
+            update_resp = self._screen.tpcclient.send_request(f"check_update")
+            # logging.info(f"update_resp: {update_resp}")
+            for child in self.button_box.get_children():
+                self.button_box.remove(child)
+            for child in self.progress_box.get_children():
+                self.progress_box.remove(child)
+            for child in self.icon_box.get_children():
+                self.icon_box.remove(child)
+            if update_resp["update_status"] == "UPDATE_AVAILABLE":
+                self.update_header.set_markup("<span size='xx-large'>" + _("New update available") + "</span>")
+                self.update_label.set_markup(f"<b>{_('Current version')}</b>: {update_resp['current_version']}\n"
+                                             f"<b>{_('Update version')}</b>: {update_resp['update_version']}")
+                self.release_notes_label.set_markup(f"<b>{_('Release notes')}</b>:\n{update_resp['release_notes']}")
+                self.icon_box.add(self.icon_update)
+                self.button_box.add(self.download_button)
+            elif update_resp["update_status"] == "DOWNLOADING":
+                self.update_header.set_markup(
+                    "<span size='xx-large'>" + _("Downloading") + f" ({int(float(update_resp['progress']))}%)</span>")
+                self.update_label.set_markup(f"<b>{_('Current version')}</b>: {update_resp['current_version']}\n"
+                                             f"<b>{_('Update version')}</b>: {update_resp['update_version']}")
+                self.release_notes_label.set_markup(f"<b>{_('Release notes')}</b>:\n{update_resp['release_notes']}")
+                self.icon_box.add(self.icon_downloading)
+                self.progress.set_fraction(float(update_resp['progress']) / 100)
+                self.progress_box.add(self.progress)
+            elif update_resp["update_status"] == "UNPACKING":
+                self.update_header.set_markup(
+                    "<span size='xx-large'>" + _("Unpacking") + f" ({int(float(update_resp['progress']))}%)</span>")
+                self.update_label.set_markup(f"<b>{_('Current version')}</b>: {update_resp['current_version']}\n"
+                                             f"<b>{_('Update version')}</b>: {update_resp['update_version']}")
+                self.release_notes_label.set_markup(f"<b>{_('Release notes')}</b>:\n{update_resp['release_notes']}")
+                self.icon_box.add(self.icon_unpacking)
+                self.progress.set_fraction(float(update_resp['progress']) / 100)
+                self.progress_box.add(self.progress)
+            elif update_resp["update_status"] == "INSTALLED":
+                self.update_header.set_markup("<span size='xx-large'>" + _("Update ready") + "</span>")
+                self.update_label.set_markup(f"<b>{_('Current version')}</b>: {update_resp['current_version']}\n"
+                                             f"<b>{_('Update version')}</b>: {update_resp['update_version']}")
+                self.release_notes_label.set_markup(f"<b>{_('Release notes')}</b>:\n{update_resp['release_notes']}")
+                self.icon_box.add(self.icon_installed)
+                self.progress.set_fraction(1)
+                self.progress_box.add(self.progress)
+                self.button_box.add(self.update_button)
+                self.update_button.set_sensitive(True)
+            elif update_resp["update_status"] == "UP_TO_DATE":
+                self.update_header.set_markup("<span size='xx-large'>" + _("System is up to date") + "</span>")
+                self.update_label.set_markup(f"<b>{_('Current version')}</b>: {update_resp['current_version']}")
+                self.release_notes_label.set_label("")
+                self.icon_box.add(self.icon_ok)
+                self.button_box.add(self.continue_button)
+            elif update_resp["update_status"] == "DOWNLOAD_FAILED":
+                self.update_header.set_markup("<span size='xx-large'>" + _("Download failed") + "</span>")
+                self.update_label.set_markup(f"<b>{_('Current version')}</b>: {update_resp['current_version']}\n"
+                                             f"<b>{_('Update version')}</b>: {update_resp['update_version']}")
+                self.icon_box.add(self.icon_warning)
+                self.button_box.add(self.retry_button)
+            elif update_resp["update_status"] == "UNPACKING_FAILED":
+                self.update_header.set_markup("<span size='xx-large'>" + _("Unpacking failed") + "</span>")
+                self.update_label.set_markup(f"<b>{_('Current version')}</b>: {update_resp['current_version']}\n"
+                                             f"<b>{_('Update version')}</b>: {update_resp['update_version']}")
+                self.icon_box.add(self.icon_warning)
+                self.button_box.add(self.retry_button)
+            elif update_resp["update_status"] == "USB_UPDATE_AVAILABLE":
+                self.update_header.set_markup("<span size='xx-large'>" + _("Update found on USB") + "</span>")
+                self.update_label.set_markup(f"<b>{_('Current version')}</b>: {update_resp['current_version']}\n"
+                                             f"<b>{_('Update version')}</b>: {update_resp['update_version']}")
+                self.release_notes_label.set_label("")
+                self.icon_box.add(self.icon_update_usb)
+                self.button_box.add(self.install_usb_button)
+            elif update_resp["update_status"] == "USB_UNPACKING":
+                self.update_header.set_markup(
+                    "<span size='xx-large'>" + _("Unpacking") + f" ({int(float(update_resp['progress']))}%)</span>")
+                self.update_label.set_markup(f"<b>{_('Current version')}</b>: {update_resp['current_version']}\n"
+                                             f"<b>{_('Update version')}</b>: {update_resp['update_version']}\n")
+                self.release_notes_label.set_label("")
+                self.icon_box.add(self.icon_unpacking_usb)
+                self.progress.set_fraction(float(update_resp['progress']) / 100)
+                self.progress_box.add(self.progress)
+            elif update_resp["update_status"] == "USB_INSTALLED":
+                self.update_header.set_markup("<span size='xx-large'>" + _("USB update ready") + "</span>")
+                self.update_label.set_markup(f"<b>{_('Current version')}</b>: {update_resp['current_version']}\n"
+                                             f"<b>{_('Update version')}</b>: {update_resp['update_version']}")
+                self.release_notes_label.set_label("")
+                self.icon_box.add(self.icon_installed_usb)
+                self.button_box.add(self.update_button)
+                self.update_button.set_sensitive(True)
+            else:
+                self.update_header.set_markup("")
+                self.update_label.set_label("")
+                self.release_notes_label.set_label("")
+        except Exception as e:
+            logging.error(e)
+            self.update_header.set_markup("")
+            self.update_label.set_label("")
+
+        # self._screen.close_popup_message()
+        self.content.show_all()
+
+    def download(self, widget):
+        self._screen.tpcclient.send_request(f"download_update","POST")
+
+    def perform_update(self, widget):
+        self._screen.tpcclient.send_request("perform_update", "POST")
+
+    def refresh_omaha(self, widget):
+        self._screen.tpcclient.send_request(f"refresh_updater", "POST")
+
+    def install_usb_update(self, widget):
+        self._screen.tpcclient.send_request(f"install_usb_update","POST")
 
     def continue_pressed(self, widget):
         self.wizard_manager.set_step(Done(self._screen))

cf73145f262d1651731819b4cccfa4c8f6a5f507
Michal Tomek <tomekmichalhu@seznam.cz>
2025-07-17 09:30:41 +0200

    Handle gcodes without thumbnail without error

--- ks_includes/screen_panel.py
+++ ks_includes/screen_panel.py
@@ -48,17 +48,20 @@ class ScreenPanel:
             self._screen._ws.klippy.emergency_stop()
 
     def get_file_image(self, filename, width=None, height=None, small=False):
-        if not self._files.has_thumbnail(filename):
-            return None
-        loc = self._files.get_thumbnail_location(filename, small)
-        if loc is None:
-            return None
-        width = width if width is not None else self._gtk.img_width
-        height = height if height is not None else self._gtk.img_height
-        if loc[0] == "file":
-            return self._gtk.PixbufFromFile(loc[1], width, height)
-        if loc[0] == "http":
-            return self._gtk.PixbufFromHttp(loc[1], width, height)
+        try:
+            if not self._files.has_thumbnail(filename):
+                return None
+            loc = self._files.get_thumbnail_location(filename, small)
+            if loc is None:
+                return None
+            width = width if width is not None else self._gtk.img_width
+            height = height if height is not None else self._gtk.img_height
+            if loc[0] == "file":
+                return self._gtk.PixbufFromFile(loc[1], width, height)
+            if loc[0] == "http":
+                return self._gtk.PixbufFromHttp(loc[1], width, height)
+        except:
+            pass
         return None
 
     def menu_item_clicked(self, widget, panel_type, item):
